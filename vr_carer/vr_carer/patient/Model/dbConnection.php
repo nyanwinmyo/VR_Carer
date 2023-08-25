5b0338ef5598a159a5083a10da087d5be9b71c26
nyanwinmyo <nyanwinmyo@gmail.com>
2023-08-26 01:40:17 +0630

    ...

--- vr_carer/vr_carer/patient/Model/dbConnection.php
+++ vr_carer/vr_carer/patient/Model/dbConnection.php
@@ -1,9 +1,9 @@
 <?php
-$hostname = "hosting-vrcarer-do-user-14484910-0.b.db.ondigitalocean.com";
+$hostname = "hosting-beyond-do-user-14484910-0.b.db.ondigitalocean.com";
 $port = "25060";
 $dbname = "vr_carer";
 $username = "doadmin";
-$password = "AVNS_9xYTMVdqbaDqTX5bFGO";
+$password = "AVNS_odSp7G5IWykGCI5O0CB";
 $pdo = new PDO(
     "mysql:host=$hostname;port=$port;dbname=$dbname", 
 $username, $password);
